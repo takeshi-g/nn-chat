a9f33cf51d807bbfbf46b30f325e60588c2d8be5
takeshi-g <tksh@gmail.com>
2023-01-24 06:03:47 +0900

    httpサーバをモジュールに分割して実装

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 "use strict";
 const http = require("http");
-const server = http.createServer((req, res) => {
-  const now = Date.now();
-  const expiresDate = new Date(now + 30 * 86400 * 1000);
-  res.setHeader("Content-Type", "text/plain;charset=utf-8");
-  res.setHeader(
-    "Set-Cookie",
-    `last_access=${now};expires=${expiresDate.toUTCString()}`
-  );
-  const last_access_time = req.headers.cookie
-    ? parseInt(req.headers.cookie.split("=")[1])
-    : now;
-  res.end(new Date(last_access_time).toString());
-});
+const router = require("./lib/router");
+
+const server = http
+  .createServer((req, res) => {
+    router.route(req, res);
+  })
+  .on("error", (e) => {
+    console.error("Server Error: ", e);
+  })
+  .on("clientError", (e) => {
+    console.error("Client Error: ", e);
+  });
+
 const port = 8000;
 server.listen(port, () => {
   console.info(`Listening on ${port}`);
