3f20d8e81492778de608f7de13cff16a7661bba0
takeshi-g <tksh@gmail.com>
2023-01-24 13:29:50 +0900

    投稿を一覧表示, 404ノットファウンド, 400バッドリクエストを実装

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 "use strict";
 const http = require("http");
+const auth = require("http-auth");
 const router = require("./lib/router");
 
+const basic = auth.basic({
+  realm: "Enter username and password.",
+  file: "./users.htpasswd",
+});
+
 const server = http
-  .createServer((req, res) => {
-    router.route(req, res);
-  })
+  .createServer(
+    basic.check((req, res) => {
+      router.route(req, res);
+    })
+  )
   .on("error", (e) => {
     console.error("Server Error: ", e);
   })
